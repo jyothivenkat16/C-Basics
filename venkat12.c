bf32a48a1a0866c870beccec2a88e445a14207dd
jyothivenkat16 <92291262+jyothivenkat16@users.noreply.github.com>
2021-11-02 12:02:02 +0530

    Add files via upload

---
+++ venkat12.c
@@ -0,0 +1,19 @@
+/* All arthimetic operations*/
+#include<stdio.h>
+void main()
+{
+	int a,b,s,r,u,o,k;
+	a=11;
+	b=21;
+	s=a+b;
+	r=a-b;
+	u=a*b;
+	o=a/b;
+	k=a%b;
+	
+	printf("sum of %d and %d is %d\n",a,b,s);
+	printf("Difference of %d and %d is %d\n",a,b,r);
+	printf("product of %d and %d is %d\n",a,b,u);
+	printf("quotient of %d and %d is %d\n",a,b,o);
+	printf("remainder of %d and %d is %d\n",a,b,k);
+}
